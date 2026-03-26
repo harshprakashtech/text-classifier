ecc1efae3ec5e0ac7d65b85c8108c29c2575f215
Harsh Prakash <harshprakash1999@gmail.com>
2026-03-26 19:03:50 +0530

    feat: Fix type bug in encode.py.

--- src/core/encode.py
+++ src/core/encode.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 # Encode text into a vector (0 and 1)
-def encode_text(text: str, vocabulary: dict[str, int]) -> list[int]:
-    vector: list[int] = np.zeros(len(vocabulary)) # [0, 0, 0, 0, 0, 0]
+def encode_text(text: str, vocabulary: dict[str, int]) -> np.ndarray:
+    vector: np.ndarray  = np.zeros(len(vocabulary)) # [0, 0, 0, 0, 0, 0]
 
     # Flip 0 -> 1 for words in text
     for word in text.split():
